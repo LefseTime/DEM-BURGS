afc47474b7986a26e1ccfbe9a429f09fab2943b3
LefseTime <36971616+LefseTime@users.noreply.github.com>
2018-07-24 19:45:49 -0500

    basic file setup

---
+++ config/orm.js
@@ -0,0 +1,15 @@
+const connection = require("../config/connection.js");
+
+const orm = {
+//stuff to export goes in here
+    // * In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
+
+    //  * `selectAll()`
+    //  * `insertOne()`
+    //  * `updateOne()`
+
+
+}
+
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Jucy Lucy');
+INSERT INTO burgers (burger_name) VALUES ('Peanut Butter Burger');
+INSERT INTO burgers (burger_name) VALUES ('Literally Just a Pile of Meat');
